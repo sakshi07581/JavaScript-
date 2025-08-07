566e28189e08ba5cdc9c6cac4ac0f1ac89339971
sakshi karn <155375468+sakshi07581@users.noreply.github.com>
2025-08-07 19:18:05 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+//let score = 55 //(in number)
+//let score = "55" //(string)
+//let score ="55abc" //NaN (Not a Number)
+//let score = null //(0)
+//let score = undefined //NaN
+//let score = true //1
+//let score = false //0
+let score ="sakshi"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score) // change the value in number
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber); //for checking the value is actual in string or number 
+
+
+//let isLoggedIn =1 //true
+//let isLoggedIn =0 //false
+//let isLoggedIn ="" //false
+let isLoggedIn ="sakshi" //true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+
+let someNumber =67
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
