49ab4b226ce7c31a5eb5bdba18c2ddf40ee2bdec
sakshi karn <155375468+sakshi07581@users.noreply.github.com>
2025-08-07 19:59:37 +0000

    changes for Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,6 +30,74 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber =67
 
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+ //console.log(stringNumber); //67
+ //console.log(typeof stringNumber); //string
+
+// ********Operation********//
+
+let value = 13
+let negValue = -value
+//console.log(negValue);
+
+
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*2); //4
+// console.log(2/5); //0.4
+// console.log(2**4); //16
+// console.log(5%2); //1
+
+let str1 = "Hello"
+let str2 =" Sakshi"
+let str3 = str1 + str2
+//console.log(str3); //Hello Sakshi
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+2+2); //122
+// console.log(1+"2"+2); //122
+// console.log(1+2+"2"); //32
+
+//console.log((3+4) *5 %6);
+
+// console.log(true); //true
+// console.log(+true); //1
+// console.log(+"");  //0
+//but it shouldnot use in production 
+
+// let gameCounter = 100
+// ++gameCounter; //prefix
+// console.log(gameCounter); //101
+
+// let gameCounter = 263
+// gameCounter++; //postfix
+// console.log(gameCounter); //263
+
+let a=3
+let b = a++ //postfix
+// console.log(a); //4
+// console.log(b); //3
+
+
+let x=8 
+let y = ++x //prefix
+// console.log(x); //9
+// console.log(y); //9
+
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type=conversion
+
+
+
+
+
+
+
+
+
+
+
+
+
 
