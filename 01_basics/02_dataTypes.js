36a4e0334860b43f71a0bb3bffe37ff76e26ed1e
sakshi karn <155375468+sakshi07581@users.noreply.github.com>
2025-08-07 18:41:18 +0000

    change done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+     +
+     2) //code readable should be high
+
+console.log("sakshi");
+
+let name ="sakshi"
+let age= 24
+let isLoggedIn=true
+let state;
+let country = null
+
+/*
+Data Types
+
+number=> 2 to power 53
+bigint=> for large amount of integer like in stock market
+string=>""
+boolean=> true/false
+null=> standalone value
+undefined=> not assign value
+symbol=>unique
+*/
+
+//object
+
+console.log(typeof "sakshi"); //string
+console.log(typeof age); //number
+console.log(typeof true); //boolean
+console.log(typeof null); //object
+console.log(typeof undefined);  //undefined
+
+
+
