67d01aec0034ee9afaaa875bdd07f7ad50269b48
sakshi karn <155375468+sakshi07581@users.noreply.github.com>
2025-08-08 12:07:35 +0000

    changes done

--- 01_basics/datatypes-summery.js
+++ 01_basics/datatypes-summery.js
@@ -43,11 +43,35 @@ const myFunction = function(){
 
 //console.log(typeof scorevalue);
 
-console.log(typeof myFunction); //function
+// console.log(typeof myFunction); //function
 
-console.log(typeof myObj); //object
+// console.log(typeof myObj); //object
 
 //https://262.ecma-international.org/5.1/#sec-11.4.3
 
 
+//*************Memory*************
+
+//stack = IN stack Memroy primitive type is used (it gives copy)
+//Heap = In Heap Memory non-primitive type is used(it gives reference)
+
+
+let myName = "sakshi"
+let anotherName = myName
+anotherName="hello sakshi"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne={
+    email:"sakshi12@gmail.com",
+    upi:"sakshi@yb1"
+}
+
+let userTwo = userOne
+
+userTwo.email="shri@google.com"
+
+console.log(userOne.email); // shri@google.com
+console.log(userTwo.email); //shri@google.com
 
